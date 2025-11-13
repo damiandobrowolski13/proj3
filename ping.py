976d6d113fd6b9b378215ddd3982a8285e3d27ce
Damian Vincent Dobrowolski <>
2025-11-13 10:52:35 -0600

    finishes todos for receive_one_ping() in ping.py: parses IP & ICMP pkts, calcs RTTs, & handles diff ICMP types + errors

--- ping.py
+++ ping.py
@@ -41,11 +41,64 @@ def receive_one_ping(mySocket, ID, timeout, destAddr):
         recPacket, addr = mySocket.recvfrom(1024)
 
         # TODO: read the packet and parse the source IP address, you will need this part for traceroute
+        src_ip = addr[0]
+        # 1st B of IPv4 head = v.(4b) + head_len(4b)
+        ip_head_len = (recPacket[0] & 0x0F) * 4
+        # 8th B of IPv4 head = TTL(8b)
+        ttl = recPacket[8]
+        # ICMP starts @ offset ip_head_len (b/c it's the IP payload)
+        icmp_off = ip_head_len
+        # icmp header is 1st 8 Bs of icmp pkt
+        icmp_head = recPacket[icmp_off:icmp_off+8]
+        icmp_type, icmp_code, icmp_cksum, icmp_id, icmp_seq = struct.unpack("bbHHh", icmp_head)
 
         # TODO: calculate and return the round trip time for this ping
-
         # TODO: handle different response type and error code, display error message to the user
-
+        
+        # case 1: Echo Reply (type 0) - payload has timestamp
+        if icmp_type == 0 and icmp_id == ID:
+            payload = recPacket[icmp_off + 8:]
+            if len(payload) >= 8:
+                try:
+                    ts_sent = struct.unpack("d", payload[:8])[0]
+                    rtt = time.time() - ts_sent
+                    return rtt 
+                except struct.error:
+                    return "Bad payload"
+            else:
+                return "No timestamp in payload"
+            
+        # case 2: Time Exceeded (11) or Dest Unreachable (3)
+        # routers incl original IP head + 1st 8 Bs of original payload
+        if icmp_type in (11, 3):
+            inner_off = icmp_off + 8
+            # make sure inner IP head present
+            if len(recPacket) >= inner_off + 20:
+                inner_ip_head_len = (recPacket[inner_off] & 0x0F) & 4
+                inner_icmp_off = inner_off + inner_ip_head_len
+                # ensure inner ICMP head + 8 Bs of inner payload present
+                if len(recPacket) >= inner_icmp_off + 16:
+                    try:
+                        _, _, _, inner_id = struct.unpack("bbHHh", recPacket[inner_icmp_off:inner_icmp_off+8])
+                    except struct.error:
+                        return "Bad inner ICMP"
+                    # if inner_id matches our ID, extract original timestamp & compute RTT
+                    if inner_id == ID:
+                        inner_payload = recPacket[inner_icmp_off + 8:inner_icmp_off + 16]
+                        try: 
+                            ts_sent = struct.unpack("d", inner_payload)[0]
+                            rtt = time.time() - ts_sent
+                            return rtt  
+                        except struct.error:
+                            return None
+            # return descriptive error msgs
+            if icmp_type == 3:
+                return f"Destination unreachable (code={icmp_code}) from {src_ip}"
+            else:
+                # time exceeded w/out matching inner packet
+                return f"Time exceeded from {src_ip}"
+            
+        # for other cases of ICMP types, ignore/wait for timeout
 
 def send_one_ping(mySocket, destAddr, ID):
     # Header is type (8), code (8), checksum (16), id (16), sequence (16)
